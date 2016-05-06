a3f8d43041e4a3ca9d8c9049413262abd66d6c67
Rlima <Rlima@RLIMA-WS1>
2016-05-05 21:42:48 -0300

    Add project files.

---
+++ StoreDbBackups/StoreDbBackups.fsx
@@ -0,0 +1,24 @@
+﻿open System
+open System.IO
+
+type Result<'success, 'failure> = 
+    | Success of 'success
+    | Failure of 'failure
+
+let getDaysToKeep (numberOfDaysToKeep : uint32) () = 
+    [ 0..(int numberOfDaysToKeep) ]
+    |> List.map (fun i -> DateTime.Today.AddDays(-(float i)))
+    |> Set.ofList
+
+let getFoldersToDelete getDaysToKeep backupRootPath () = 
+    let mapDateToFolderPath (date : DateTime) = Path.Combine(backupRootPath, date.ToString("dd-MM-yyyy"))
+    let allFoldersFromRoot = Directory.GetDirectories(backupRootPath) |> Set.ofArray
+    let foldersToKeep = getDaysToKeep() |> Set.map mapDateToFolderPath
+    allFoldersFromRoot - foldersToKeep
+
+let deleteOldBackups deleteFolders getFoldersToDelete () = getFoldersToDelete() |> deleteFolders
+
+let storeBackupsAndCleanOldOnes deleteOldBackups copyFilesToBackupFolder logResult () = 
+    [ deleteOldBackups()
+      copyFilesToBackupFolder() ]
+    |> logResult

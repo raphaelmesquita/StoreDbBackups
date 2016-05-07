d6cf8d5af8c6f33454838d55e51e01be132d7e2f
Raphael Lima <rmesquita@gmail.com>
2016-05-06 22:00:34 -0300

    Implementação completa do deleteOldBackups

--- StoreDbBackups/StoreDbBackups.fsx
+++ StoreDbBackups/StoreDbBackups.fsx
@@ -10,15 +10,41 @@ let getDaysToKeep (numberOfDaysToKeep : uint32) () =
     |> List.map (fun i -> DateTime.Today.AddDays(-(float i)))
     |> Set.ofList
 
-let getFoldersToDelete getDaysToKeep backupRootPath () = 
-    let mapDateToFolderPath (date : DateTime) = Path.Combine(backupRootPath, date.ToString("dd-MM-yyyy"))
+let getFoldersToDelete getDaysToKeep backupRootPath (folderFormat : string) () = 
+    let mapDateToFolderPath (date : DateTime) = Path.Combine(backupRootPath, date.ToString(folderFormat))
     let allFoldersFromRoot = Directory.GetDirectories(backupRootPath) |> Set.ofArray
     let foldersToKeep = getDaysToKeep() |> Set.map mapDateToFolderPath
     allFoldersFromRoot - foldersToKeep
 
-let deleteOldBackups deleteFolders getFoldersToDelete () = getFoldersToDelete() |> deleteFolders
+let deleteFolder folderPath = 
+    try 
+        Directory.Delete(folderPath, true)
+        Success()
+    with ex -> Failure ex.Message
 
-let storeBackupsAndCleanOldOnes deleteOldBackups copyFilesToBackupFolder logResult () = 
+let deleteFolders deleteFolder foldersToDeletePaths = 
+    let accumulateResult allResults result = 
+        match allResults, result with
+        | Failure list, Failure error -> Failure(list @ [ error ])
+        | Failure list, _ -> Failure list
+        | _, Failure error -> Failure([ error ])
+        | Success _, Success _ -> Success()
+    foldersToDeletePaths
+    |> Seq.map deleteFolder
+    |> Seq.fold accumulateResult (Success())
+
+let deleteOldBackups deleteFolders getFoldersToDelete () = getFoldersToDelete() |> deleteFolders
+// config
+let numberOfDaysToKeep = 1u
+let backupRootPath = "C:\Temp"
+let folderFormat = "yyyy-MM-dd"
+// composição
+let getDaysToKeep' = getDaysToKeep numberOfDaysToKeep
+let getFoldersToDelete' = getFoldersToDelete getDaysToKeep' backupRootPath folderFormat
+let deleteFolders' = deleteFolders deleteFolder
+let deleteOldBackups' = deleteOldBackups deleteFolders' getFoldersToDelete'
+
+let storeBackupsAndCleanOldOnes deleteOldBackups copyFilesToBackupFolder notifyResult () = 
     [ deleteOldBackups()
       copyFilesToBackupFolder() ]
-    |> logResult
+    |> notifyResult
